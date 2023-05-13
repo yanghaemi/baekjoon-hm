c00ae956901ceb5e888de24e1ab9c3866be22980
yanghaemi <heau0105@naver.com>
2023-05-13 17:16:04 +0900

    [ac] 8393

---
+++ 8393.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, res = 0;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        res = res + i;
+    }
+    cout << res;
+}
