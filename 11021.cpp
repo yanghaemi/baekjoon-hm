c47133ff54c94758d2a47fe78cc46d7e9227736f
yanghaemi <heau0105@naver.com>
2023-05-16 02:08:11 +0900

    [ac] 11021 A+B -7

---
+++ 11021.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+    vector<int> A(T);
+    vector<int> B(T);
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A.at(i) >> B.at(i);
+    }
+    for (int i = 0; i < T; i++)
+    {
+        cout << "Case #" << i + 1 << ": " << A.at(i) + B.at(i) << endl;
+    }
+}
