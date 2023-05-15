f67d03b127d702504e00895bc1fee34269dc51cd
yanghaemi <heau0105@naver.com>
2023-05-16 01:39:03 +0900

    [ac] 10171 고양이

---
+++ 10171.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+}
