007c376b5308195bb4d673de6eec22d82d68dfaf
yanghaemi <heau0105@naver.com>
2023-05-16 01:07:59 +0900

    [ac] 10869

---
+++ 10869.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+}
