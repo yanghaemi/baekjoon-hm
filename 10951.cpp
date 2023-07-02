c82e3cd404e5904fb84a39a3de512245f501b68c
yanghaemi <heau0105@naver.com>
2023-07-03 03:34:27 +0900

    [ac] 10951

---
+++ 10951.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#define endl "\n"
+
+int main()
+{
+    int a, b;
+    while (!(cin >> a >> b).eof())
+    {
+
+        cout << a + b << '\n';
+    }
+}
