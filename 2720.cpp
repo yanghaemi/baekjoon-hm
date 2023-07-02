e1cd1749133f7b2465620abfbd60954578792049
yanghaemi <heau0105@naver.com>
2023-07-03 02:35:57 +0900

    [ac] 1720

--- 2720.cpp
+++ 2720.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+#define endl "\n"
 
 int main()
+{
+    int t;
+    cin >> t;
+
+    vector<int> v(t), q(t), d(t), n(t), p(t);
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> v[i];
+    }
+
+    for (int i = 0; i < t; i++)
+    {
+        q[i] = v[i] / 25;
+        v[i] %= 25;
+        d[i] = v[i] / 10;
+        v[i] %= 10;
+        n[i] = v[i] / 5;
+        v[i] %= 5;
+        p[i] = v[i];
+    }
+    for (int i = 0; i < t; i++)
+    {
+        cout << q[i] << " " << d[i] << " " << n[i] << " " << p[i] << '\n';
+    }
+}
