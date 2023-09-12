53a8f8d9f4f0519ee1e7595aa9ede827ab3affd9
yanghaemi <heau0105@naver.com>
2023-09-13 03:46:06 +0900

    [ac] 25083

---
+++ 25083.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "         ,r'\""
+            "7\nr`-_   ,'  ,/\n \\. \""
+            ". L_r'\n   `~\\/\n      |\n      |";
+}
