0cf82a233cac34ff08b843f79b4e4110a9112cb2
yanghaemi <heau0105@naver.com>
2023-10-02 20:39:32 +0900

    [ac] 7576

--- 7576.cpp
+++ 7576.cpp
@@ -1,27 +1,90 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+int m, n, day = 0, flag = 0;
+queue<pair<int, int>> q;
+queue<pair<int, int>> next_q;
+
+void BFS(vector<vector<int>> &v)
+{
+    int move_x[4] = {-1, 0, 1, 0};
+    int move_y[4] = {0, -1, 0, 1};
+    int y = q.front().first, x = q.front().second;
+
+    int xx = 0, yy = 0;
+
+    for (int i = 0; i < 4; i++)
+    {
+
+        xx = x + move_x[i], yy = y + move_y[i];
+        if (xx >= 0 && xx < m && yy >= 0 && yy < n)
+        {
+            if (v[yy][xx] == 0)
+            {
+                next_q.push(make_pair(yy, xx));
+                v[yy][xx] = 1;
+            }
+            else
+                continue;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    q.pop();
+}
 
 int main()
 {
-    int m, n;
     cin >> m >> n;
     int num;
 
-    queue<pair<int, int>> q;
-
-    vector<vector<int>> v;
+    vector<vector<int>> v(n, vector<int>(m, 0));
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             cin >> num;
-            v[n].push_back(num);
+            v[i][j] = num;
 
             if (num == 1)
-                q.push(pair<int, int>(n, m));
+            {
+                q.push(make_pair(i, j));
+            }
         }
     }
+    while (!q.empty())
+        BFS(v);
+
+    while (!next_q.empty())
+    {
+        swap(q, next_q);
+
+        while (!q.empty())
+        {
+
+            BFS(v);
+        }
+        day++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (v[i][j] == 0)
+            {
+                cout << "-1";
+                return 0;
+            }
+        }
+    }
+
+    cout << day;
+    return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,40 +1,5 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main()
-{
-    int n, cnt = 0;
-    cin >> n;
-
-    vector<int> v(n + 1);
-
-    v[11] = 3;
-
-    for (int i = 1; i <= n; i++)
-    {
-        if (i == 4 || i == 7)
-            v[i] = -1;
-        else if (i % 5 == 0)
-            v[i] = i / 5;
-        else if (i % 3 == 0 && i <= 12)
-            v[i] = i / 3;
-        else if (i % 8 == 0)
-            v[i] = 2 * (i / 8);
-        else if (i > 12)
-        {
-            int m = i;
-            while (m - 8 >= 3 && m - 8 != 7 && m - 8 != 4 && m - 8 != 12)
-            {
-
-                m -= 8;
-                if (m % 5 == 0)
-                    break;
-            }
-            v[i] = v[i - m] + v[m];
-        }
-    }
-    // cout << '\n';
-
-    cout << v[n];
-}
+if (v[x - 1][y] == -1 && v[x][y - 1] == -1 && v[x + 1][y] == -1 && v[x][y + 1] == -1)
+    // {
+    //     flag = 1;
+    //     return;
+    // }
