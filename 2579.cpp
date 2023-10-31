797f19d1af7cfd889499b3c4e8e3a9e678d4dc97
yanghaemi <heau0105@naver.com>
2023-10-31 10:47:42 +0900

    10718) We love kriii

--- 2579.cpp
+++ 2579.cpp
@@ -17,7 +17,8 @@ void DP(vector<int> &v)
     }
     else
     {
-        // cout << "현재 v:" << n << ":" << v[n] + v[n - 1] << ", " << v[n] + v[n - 2] << '\n';
+        // 같은 것도 고려해야됨 ~~~ 3 30 30 10
+        //  cout << "현재 v:" << n << ":" << v[n] + v[n - 1] << ", " << v[n] + v[n - 2] << '\n';
         if (v[n] + v[n - 1] > v[n] + v[n - 2])
         {
             res += v[n - 1];
@@ -32,7 +33,7 @@ void DP(vector<int> &v)
         }
     }
 
-    if (n - 1 > 0)
+    if (n - 1 >= 0)
         DP(v);
     else
     {
@@ -49,6 +50,12 @@ int main()
     {
         cin >> v[i]; // 계단 data
     }
+    if (n == 1)
+    {
+        cout << v[1];
+        return 0;
+    }
     res = v[n];
+    DP(v);
     cout << res;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,54 @@
-(str[i] - '0') + 1
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int res = 0;
+int n;
+int flag = 0;
+
+void DP(vector<int> &v)
+{
+
+    if (flag == 1)
+    {
+        res += v[n - 2];
+        n = n - 2;
+        flag = 0;
+    }
+    else
+    {
+        cout << "현재 v:" << n << ":" << v[n] + v[n - 1] << ", " << v[n] + v[n - 2] << '\n';
+        if (v[n] + v[n - 1] > v[n] + v[n - 2])
+        {
+            res += v[n - 1];
+            flag++;
+            n--;
+        }
+        else
+        {
+            res += v[n - 2];
+            n = n - 2;
+            flag = 0;
+        }
+    }
+
+    if (n - 1 > 0)
+        DP(v);
+    else
+    {
+        return;
+    }
+}
+
+int main()
+{
+
+    cin >> n; // 계단 개수
+    vector<int> v(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i]; // 계단 data
+    }
+    res = v[n];
+    cout << res;
+}
