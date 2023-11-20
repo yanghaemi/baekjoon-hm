5a0ae380a6256eec2542f06770fdf1e9ce95ef3b
yanghaemi <heau0105@naver.com>
2023-11-21 01:28:34 +0900

    요세푸스 문제

--- 1158.cpp
+++ 1158.cpp
@@ -47,6 +47,7 @@ void deletenode(int k)
     {
         node *temp = head;
         head = head->next;
+        cout << temp->data;
         delete temp;
         return;
     }
@@ -56,6 +57,7 @@ void deletenode(int k)
     }
     node *temp = cur->next;
     cur->next = cur->next->next;
+
     head = cur->next;
     cout << temp->data;
     delete temp;
@@ -65,41 +67,30 @@ void deletenode(int k)
     return;
 }
 
-void getList()
-{
-    node *cur = head;
-
-    for (int i = 0; i <= Copy_n; i++)
-    {
-        cout << cur->data << "->";
-        cur = cur->next;
-    }
-    cout << '\n';
-}
-
 int main()
 {
-
-    int i;
     cin >> n >> k;
     Copy_n = n;
-    headnode(1); // 1은 헤드로 삽입
-    if (i >= 2)
+    if (n == 1)
     {
-        for (i = 2; i <= n; i++)
+        cout << "<1>";
+        return 0;
+    }
+    headnode(1); // 1은 헤드로 삽입
+    if (n >= 2)
+    {
+        for (int i = 2; i <= n; i++)
         {
             insert(i); // 2~n까지 연결리스트 연결
         }
     }
 
-    // getList();
     cout << "<";
 
     for (int j = 0; j < n - 1; j++)
     {
         deletenode(k);
         cout << ", ";
-        // getList();
     }
     deletenode(k);
     cout << ">";
